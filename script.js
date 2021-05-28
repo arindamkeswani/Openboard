12414090c942046f58e0e8aafb1b8102635be05a
Arindam <arindamkeswani@gmail.com>
2021-05-29 01:41:58 +0530

    Added Undo and Redo functionalities

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let board = document.getElementById("board");
 let menu = document.querySelector(".combined-menu");
-let toolbar= document.querySelector(".toolbar");
+let toolbar = document.querySelector(".toolbar");
 let pencilBtn = document.querySelector(".fa-pencil-alt");
 let pencilOpt = document.querySelector(".pencil-options");
 let colorBtn = document.querySelector("#color");
@@ -12,6 +12,16 @@ let eraserMeter = document.querySelector("#rubber-size");
 
 let noteBtn = document.querySelector("#note");
 let mainContainer = document.querySelector(".main-container");
+
+let undoBtn=document.querySelector("#undo");
+let undo_array = [];
+let undo_idx = 0;
+
+let redoBtn=document.querySelector("#redo");
+let redo_array = [];
+let redo_idx = -1;
+
+
 board.height = window.innerHeight;
 board.width = window.innerWidth;
 
@@ -29,26 +39,26 @@ tool.lineCap = "round";
 
 //pencil functions
 let pencilOptActive = false; //check if pencil option is active or not
-let penColor="black";
+let penColor = "black";
 let penSize = 5;
 pencilBtn.addEventListener("click", function () {
     console.log(tool.lineWidth);
-    pencilOptActive=!pencilOptActive;
+    pencilOptActive = !pencilOptActive;
 
-    if(pencilOptActive==true){
+    if (pencilOptActive == true) {
         pencilOpt.style.opacity = "1";
         pencilOpt.style.zIndex = "3";
         // tool.strokeStyle = "black";
-    }else{
+    } else {
         pencilOpt.style.opacity = "0";
         pencilOpt.style.zIndex = "-1";
         tool.strokeStyle = penColor;
     }
-    
+
     tool.strokeStyle = penColor;
     tool.lineWidth = penSize;
     let isMouseDown = false;
-    document.body.addEventListener("mousedown", function (e) {
+    board.addEventListener("mousedown", function (e) {
         // console.log("Down");
         x = e.clientX;
         y = e.clientY;
@@ -57,7 +67,7 @@ pencilBtn.addEventListener("click", function () {
         tool.moveTo(x, y);
         isMouseDown = true;
     })
-    document.body.addEventListener("mousemove", function (e) {
+    board.addEventListener("mousemove", function (e) {
         x = e.clientX;
         y = e.clientY;
         y = getCoordinates(y);
@@ -69,7 +79,7 @@ pencilBtn.addEventListener("click", function () {
         }
 
     })
-    document.body.addEventListener("mouseup", function (e) {
+    board.addEventListener("mouseup", function (e) {
         // console.log("Up");
         x = e.clientX;
         y = e.clientY;
@@ -77,17 +87,25 @@ pencilBtn.addEventListener("click", function () {
 
         // tool.stroke();
         isMouseDown = false;
+
+        if(e){
+            undo_array.push(tool.getImageData(0, 0, board.height, board.width));
+            undo_idx += 2;
+        }
+        // console.log(undo_array);
+        
     })
 
 
 })
-
+pencilBtn.click();
+pencilBtn.click();
 //change pen size
-rangeMeter.addEventListener("click", function(){
-    
-    penSize=rangeMeter.value;
-    tool.lineWidth=penSize;
-    
+rangeMeter.addEventListener("click", function () {
+
+    penSize = rangeMeter.value;
+    tool.lineWidth = penSize;
+
 })
 
 //change colour
@@ -101,20 +119,20 @@ colorBtn.addEventListener("input", function () {
 
 //eraser
 let eraserOptActive = false;
-let eraserColor="white";
+let eraserColor = "white";
 let eraserSize = 5;
 eraserBtn.addEventListener("click", function () {
     console.log(tool.lineWidth);
 
-    pencilOptActive=false;
+    pencilOptActive = false;
     pencilOpt.style.opacity = "0";
     pencilOpt.style.zIndex = "-1";
 
     eraserOptActive = !eraserOptActive;
-    if(eraserOptActive){
+    if (eraserOptActive) {
         eraserMeterCont.style.opacity = "1";
         eraserMeterCont.style.zIndex = "3";
-    }else{
+    } else {
         eraserMeterCont.style.opacity = "0";
         eraserMeterCont.style.zIndex = "-1";
     }
@@ -122,7 +140,7 @@ eraserBtn.addEventListener("click", function () {
     tool.strokeStyle = eraserColor;
     tool.lineWidth = eraserSize;
     let isMouseDown = false;
-    document.body.addEventListener("mousedown", function (e) {
+    board.addEventListener("mousedown", function (e) {
         // console.log("Down");
         x = e.clientX;
         y = e.clientY;
@@ -131,7 +149,7 @@ eraserBtn.addEventListener("click", function () {
         tool.moveTo(x, y);
         isMouseDown = true;
     })
-    document.body.addEventListener("mousemove", function (e) {
+    board.addEventListener("mousemove", function (e) {
         x = e.clientX;
         y = e.clientY;
         y = getCoordinates(y);
@@ -143,7 +161,7 @@ eraserBtn.addEventListener("click", function () {
         }
 
     })
-    document.body.addEventListener("mouseup", function (e) {
+    board.addEventListener("mouseup", function (e) {
         // console.log("Up");
         x = e.clientX;
         y = e.clientY;
@@ -151,27 +169,31 @@ eraserBtn.addEventListener("click", function () {
 
         // tool.stroke();
         isMouseDown = false;
+
+        undo_array.push(tool.getImageData(0, 0, board.width, board.height));
+        undo_idx += 2;
+        // console.log(undo_array);
     })
 
 
 })
 
-eraserMeter.addEventListener("click", function(){
-    
-    eraserSize=eraserMeter.value;
+eraserMeter.addEventListener("click", function () {
+
+    eraserSize = eraserMeter.value;
     console.log(eraserSize);
-    tool.lineWidth=eraserSize;
+    tool.lineWidth = eraserSize;
     console.log(tool.lineWidth);
-    
+
 })
 
 //sticky notes
-noteBtn.addEventListener("click", function(){
-    createNote()
+noteBtn.addEventListener("click", function () {
+    createNote();
 })
-function createNote(){
+function createNote() {
     // console.log(x);
-    let taskContainer=document.createElement("div");
+    let taskContainer = document.createElement("div");
 
     taskContainer.setAttribute("class", "sticky-note");
     taskContainer.innerHTML = `     <div class="nav">
@@ -181,50 +203,107 @@ function createNote(){
                                     <div class="writing-pad" contenteditable="true">
                                         
                                     </div>`;
-                                // <textarea></textarea>
+    // <textarea></textarea>
     mainContainer.appendChild(taskContainer);
 
-    
+
     taskContainer.style.position = "absolute";
 
-    let noteNav=taskContainer.childNodes[1];
+    let noteNav = taskContainer.childNodes[1];
     // console.log(noteNav.childNodes[3]);
-    noteNav.onmousedown = function(){
+    noteNav.onmousedown = function () {
         dragValue = taskContainer;
     }
-    document.onmouseup = function(e){
-        dragValue=null;
+    document.onmouseup = function (e) {
+        dragValue = null;
     }
-    document.onmousemove = function(e){
-        if(dragValue!=null){
-            var x= e.pageX;
-            var y= e.pageY;
-    
-            dragValue.style.left = x+"px";
-            dragValue.style.top = y+"px";
+    document.onmousemove = function (e) {
+        if (dragValue != null) {
+            var x = e.pageX;
+            var y = e.pageY;
+
+            dragValue.style.left = x + "px";
+            dragValue.style.top = y + "px";
         }
-        
+
     }
-    
+
     let mini = noteNav.childNodes[1];
     let close = noteNav.childNodes[3];
     let pad = taskContainer.childNodes[3];
 
-    mini.addEventListener("click", function(){
-        if(pad.style.display != "none"){
+    mini.addEventListener("click", function () {
+        if (pad.style.display != "none") {
             pad.style.display = "none";
-        }else{
+        } else {
             pad.style.display = "block";
         }
-        
+
     })
-    
-    close.addEventListener("click",function(){
+
+    close.addEventListener("click", function () {
         taskContainer.remove();
     })
 }
 
+//undo
 
+undoBtn.addEventListener("click", function(){
+    console.log("Clicked Undo");
+    undo();
+})
+
+function undo(){
+    
+    if(undo_array.length<=0){
+        alert("Canvas is empty");
+    }else{
+        undo_idx -=2;
+        
+        redo_array.push(undo_array.pop());
+        redo_array.push(undo_array.pop());
+        tool.clearRect(0, 0, board.width, board.height);
+        
+        
+        // undo_array.pop()
+        // console.log(undo_array);
+        for(let i =0;i<undo_array.length;i+=2){ //putting it in loop fixed "Not Image type error"
+            tool.putImageData(undo_array[i],0,0);
+        }
+        
+    }
+}
+
+
+redoBtn.addEventListener("click", function(){
+    console.log("Clicked redo");
+    redo();
+})
+
+function redo(){
+    
+    if(redo_array.length<=0){
+        alert("Reached final stroke.");
+    }else{
+        // redo_idx -=2;
+        
+        
+        
+        tool.clearRect(0, 0, board.width, board.height);
+        console.log(redo_array);
+        for(let i =0;i<1;i+=1){ //putting it in loop fixed "Not Image type error"
+            // tool.putImageData(redo_array.pop(),0,0);
+            
+            undo_array.push(redo_array.pop());
+            undo_array.push(redo_array.pop());
+            
+            for(let i =0;i<undo_array.length;i+=1){ //putting it in loop fixed "Not Image type error"
+                tool.putImageData(undo_array[i],0,0);
+            }
+        }
+        
+    }
+}
 
 
 function getCoordinates(initialY) {
